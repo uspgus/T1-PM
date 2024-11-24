d334c6891de4696cc74a6993afc34e869860aaf3
Gustavo Ferreira <gusferreira1203@usp.br>
2024-11-24 11:36:19 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Manages input and presentation, delegating mathematical programming logic to other files.
+
+# Gustavo Ferreira, Murilo Zabott
+# 2nd Semester, 2024
+# University of São Paulo, Institute of Mathematics and Computer Science
