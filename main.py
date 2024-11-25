66014fe3f0f779ff5301fddd5a0eef1a7e966490
Gustavo Ferreira <gusferreira1203@usp.br>
2024-11-25 19:51:32 -0300

    inicio do modelo

--- main.py
+++
@@ -1,5 +0,0 @@
-# Manages input and presentation, delegating mathematical programming logic to other files.
-
-# Gustavo Ferreira, Murilo Zabott
-# 2nd Semester, 2024
-# University of São Paulo, Institute of Mathematics and Computer Science
